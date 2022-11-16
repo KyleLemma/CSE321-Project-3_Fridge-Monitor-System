1b952efbfb66f7a2179f1265afb15c849517d5df
KyleLemma <89815745+KyleLemma@users.noreply.github.com>
2022-11-16 17:10:01 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,45 +25,46 @@ References:
 #include "DHT11.h"
 #include <cstdio>
 #include <string>
+#include <iostream>
 
+using namespace std;
 //Flag if the button to engage the system has been pushed or not
 //Use a boolean to be the argument of the while loop
 void systemError();
+void checkMetrics();
 
-InterruptIn buttonPush(PB_8,PullDown);
+InterruptIn buttonPushA(PB_8,PullDown);
+InterruptIn buttonPushB(PB_8,PullDown);
 CSE321_LCD lcd(16,2,LCD_5x10DOTS,PC_1,PC_0);
 DHT11 monitor(PB_1);
-#include <iostream>
-#include <cstdio>
-using namespace std;
+
+DigitalOut rLED(PA_3);
+DigitalOut gLED(PA_2);
+DigitalOut buzzer(PC_8,PullDown);
+
 
 
 int main()
 {
-    // GPIOB -> MODER = 0x00; //0000
-    //GPIOB -> MODER &= ~(0x1100); 
     //Clock turned on for GPIO B and C
-    RCC->AHB2ENR |= 0x6;
+    RCC->AHB2ENR |= 0x7;
+    
     lcd.begin();
+    bool check = false;
+    
     while(1){
         
-        // lcd.begin();
-        lcd.begin();
-        monitor.read();
-        string tempString = to_string(monitor.getCelsius()).c_str();
-        string reading = "Temp is: ";
-        string result = reading + tempString + " C";
-        lcd.print(result.c_str());
-        lcd.setCursor(0, 1);
-        string humidString = to_string(monitor.getHumidity()).c_str();
-        string humReading = "Humidity is: ";
-        string humRes = humReading + humidString;
-        lcd.print(humRes.c_str());
-        // printf("%f \n" , 1.2345);
-        wait_us(10000000);
-        lcd.clear();
+        rLED = 1;
+        checkMetrics();
+        //Wait 2 seconds for monitor read to reset
+        wait_us(2000000);
+
         //Waiting for the system to either be enganged or disengaged by the matrix keypad   
-        //while(button has been pressed to arm){}
+        while(check){
+            checkMetrics();
+            rLED = 0;
+            gLED = 1;
+        }
             //Monitor the temperature and humidity levels
 
             //If either the temp or humid gets too high or low go to systemError
@@ -72,6 +73,29 @@ int main()
 }
 
 
+
+void checkMetrics(){
+        
+        monitor.read();
+        int temp = monitor.getCelsius();
+        string tempString = to_string(temp).c_str();
+        string reading = "Temp is: ";
+        string result = tempString + " C";
+        
+        lcd.print(reading.c_str());
+        lcd.print(result.c_str());
+        lcd.setCursor(0, 1);
+
+        int humidity = monitor.getHumidity();
+        string humidString = to_string(humidity).c_str();
+        string humReading = "Humidity is: ";
+        string humRes = humidString+ "%";
+
+        lcd.print(humReading.c_str());
+        lcd.print(humRes.c_str());
+        lcd.home();
+
+}
 
 
 void systemError(){
